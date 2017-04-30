22cb67c1121d0e38f69edd9dbdf3e10b592a0d75
Selcher <selcher123@gmail.com>
2017-05-01 02:58:51 +0800

    Add module to search for gif images

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # gif-search
+
 A javascript module for searching gif images
+
+## Getting Started
+
+```
+import gifSearch from 'gif-search';
+
+// Search for a gif
+gifSearch.gif('cat').then(
+    gifUrl => console.log(gifUrl)
+);
+
+// Search for a random gif
+gifSearch.gifr('cat').then(
+    gifUrl => console.log(gifUrl)
+);
+```
+
+### Installing
+
+```
+npm install gif-search
+```
+
+## Built With
+
+* [Gipy API](https://api.giphy.com/)
+* [Rollup](https://github.com/rollup/rollup) - module bundler
+* [Request](https://github.com/request/request) - make http calls
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://github.com/selcher/gif-search/blob/master/CONTRIBUTING.md) for details on contributing to the repository.
+
+## Development
+
+Note:
+
+There is an [issue](https://github.com/request/request/issues/2483) with the aws-sign2 module used in the request module with rollup.
+
+To get this working in your local development environment, you may need to manually fix the issue until the fix gets published on npm.
+
+## Versioning
+
+The project uses [Sember](http://semver.org) for versioning. To check the released versions, see the [tags on this repository](https://github.com/selcher/gif-search/releases).
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/selcher/gif-search/blob/master/LICENSE) file for details.
---
+++ src/main.js
@@ -0,0 +1,59 @@
+'use strict';
+
+import Request from 'request';
+
+/**
+ * Search for a gif using GiphyApi
+ */
+module.exports = {
+    gif: (input) => {
+
+        return new Promise((resolve, reject) => {
+
+            let keywords = input.split(' ').join('+');
+            let gifApi = [
+                'http://api.giphy.com/v1/gifs/search?',
+                `q=${keywords}`,
+                '&api_key=dc6zaTOxFJmzC&limit=1',
+                '&offset=0'
+            ].join('');
+
+            Request(gifApi, (err, response, body) => {
+                if (err) {
+                    reject(err);
+                } else if (response.statusCode === 200) {
+                    let parsedData = JSON.parse(body);
+                    let url = parsedData.data[0].images['fixed_height'].url;
+                    resolve(url);
+                } else {
+                    reject(err);
+                }
+            });
+        });
+    },
+    gifr: (input) => {
+
+        return new Promise((resolve, reject) => {
+
+            let keywords = input.split(' ').join('+');
+            let gifApi = [
+                'http://api.giphy.com/v1/gifs/random?',
+                `tag=${keywords}`,
+                '&api_key=dc6zaTOxFJmzC&limit=1',
+                '&offset=0'
+            ].join('');
+
+            Request(gifApi, (err, response, body) => {
+                if (err) {
+                    reject(err);
+                } else if (response.statusCode === 200) {
+                    let parsedData = JSON.parse(body);
+                    let url = parsedData.data.image_url;
+                    resolve(url);
+                } else {
+                    reject(err);
+                }
+            });
+        });
+    }
+};
