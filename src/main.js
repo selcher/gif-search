f63302d100f027645631c27cd030bb61318f64d0
Selcher <selcher123@gmail.com>
2018-01-04 20:10:11 +0800

    Minor code improvements

--- src/main.js
+++ src/main.js
@@ -2,58 +2,86 @@
 
 import Request from 'request';
 
+const replaceSpaceWithPlus = (str) => (
+    str.split(' ').join('+')
+);
+const api = {
+    giphy: {
+        search: {
+            url: (keywords) => {
+                return [
+                    'http://api.giphy.com/v1/gifs/search?',
+                    `q=${replaceSpaceWithPlus(keywords)}`,
+                    '&api_key=dc6zaTOxFJmzC&limit=1',
+                    '&offset=0'
+                ].join('');
+            },
+            imageUrl: (data) => (
+                data.data[0].images['fixed_height'].url
+            )
+        },
+        random: {
+            url: (keywords) => {
+                return [
+                    'http://api.giphy.com/v1/gifs/random?',
+                    `tag=${replaceSpaceWithPlus(keywords)}`,
+                    '&api_key=dc6zaTOxFJmzC&limit=1',
+                    '&offset=0'
+                ].join('');
+            },
+            imageUrl: (data) => (
+                data.data.image_url
+            )
+        }
+    }
+};
+
 /**
  * Search for a gif using GiphyApi
  */
 module.exports = {
     query: (input) => {
+        const searchApi = api.giphy.search;
 
         return new Promise((resolve, reject) => {
-
-            let keywords = input.split(' ').join('+');
-            let gifApi = [
-                'http://api.giphy.com/v1/gifs/search?',
-                `q=${keywords}`,
-                '&api_key=dc6zaTOxFJmzC&limit=1',
-                '&offset=0'
-            ].join('');
-
-            Request(gifApi, (err, response, body) => {
-                if (err) {
-                    reject(err);
-                } else if (response.statusCode === 200) {
-                    let parsedData = JSON.parse(body);
-                    let url = parsedData.data[0].images['fixed_height'].url;
-                    resolve(url);
-                } else {
-                    reject(err);
+            Request(
+                searchApi.url(input),
+                (err, response, body) => {
+                    if (err) {
+                        reject(err);
+                    } else if (response.statusCode === 200) {
+                        resolve(
+                            searchApi.imageUrl(
+                                JSON.parse(body)
+                            )
+                        );
+                    } else {
+                        reject(err);
+                    }
                 }
-            });
+            );
         });
     },
     random: (input) => {
+        const randomApi = api.giphy.random;
 
         return new Promise((resolve, reject) => {
-
-            let keywords = input.split(' ').join('+');
-            let gifApi = [
-                'http://api.giphy.com/v1/gifs/random?',
-                `tag=${keywords}`,
-                '&api_key=dc6zaTOxFJmzC&limit=1',
-                '&offset=0'
-            ].join('');
-
-            Request(gifApi, (err, response, body) => {
-                if (err) {
-                    reject(err);
-                } else if (response.statusCode === 200) {
-                    let parsedData = JSON.parse(body);
-                    let url = parsedData.data.image_url;
-                    resolve(url);
-                } else {
-                    reject(err);
+            Request(
+                randomApi.url(input),
+                (err, response, body) => {
+                    if (err) {
+                        reject(err);
+                    } else if (response.statusCode === 200) {
+                        resolve(
+                            randomApi.imageUrl(
+                                JSON.parse(body)
+                            )
+                        );
+                    } else {
+                        reject(err);
+                    }
                 }
-            });
+            );
         });
     }
 };
