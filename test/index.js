b797b2525bf11f196c1634ef50fe4900dde5b683
Selcher <selcher123@gmail.com>
2019-06-08 15:59:22 +0900

    Update the tests

--- test/index.js
+++ test/index.js
@@ -5,20 +5,41 @@ const isValidGifUrl = (url) => (
     url.match(/\/.*?.gif/g) ? true : false
 );
 
-test('Gif search', async t => {
-    const response = gifSearch.query('cat');
-    const gifUrl = await response;
+test('Set API Key', async t => {
+    const defaultAPIKey = gifSearch.getAPIKey();
+    const newAPIKey = 'test';
 
-    t.true(isValidGifUrl(gifUrl));
+    gifSearch.setAPIKey(newAPIKey);
+
+    t.true(gifSearch.getAPIKey() !== defaultAPIKey);
+
+    gifSearch.setAPIKey(defaultAPIKey);
+});
+
+test('Gif search', async t => {
+    try {
+        const response = gifSearch.query('cat');
+        const gifUrl = await response;
+
+        t.true(isValidGifUrl(gifUrl));
+    }
+    catch(e) {
+        t.true(false, e);
+    }
 });
 
 test('Random gif search', async t => {
-    const response1 = gifSearch.random('cat');
-    const response2 = gifSearch.random('cat');
-    const gifUrl1 = await response1;
-    const gifUrl2 = await response2;
+    try {
+        const response1 = gifSearch.random('cat');
+        const response2 = gifSearch.random('cat');
+        const gifUrl1 = await response1;
+        const gifUrl2 = await response2;
 
-    t.true(isValidGifUrl(gifUrl1));
-    t.true(isValidGifUrl(gifUrl2));
-    t.true(gifUrl1 !== gifUrl2);
+        t.true(isValidGifUrl(gifUrl1));
+        t.true(isValidGifUrl(gifUrl2));
+        t.true(gifUrl1 !== gifUrl2);
+    }
+    catch(e) {
+        t.true(false, e);
+    }
 });
